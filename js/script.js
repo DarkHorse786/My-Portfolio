02d45f4739e9956e641e3fc766b37c4f5e84d731
DarkHorse786 <samiullahwaheed@gmail.com>
2024-08-25 08:18:02 -0700

    info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // =====   typing animation   =====
 var typed = new Typed(".typing",{
-    strings:["Web Designer","Web Developer","Graphic Designer","Software Developer"],
+    strings:["Software Engineer","Web Developer","Graphic Designer","Software Developer"],
     typeSpeed:100,
     BackSpeed: 60,
     loop:true
